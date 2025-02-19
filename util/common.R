9eebd23cb039be8a1a6825af8f3c832af1658839
Jörg Müller <joerg@toytic.com>
2025-02-19 16:11:46 +0100

    updated ch06, table_frq

--- util/common.R
+++ util/common.R
@@ -1,5 +1,7 @@
 set.seed(2024)
 
+library(flextable)
+
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
@@ -27,7 +29,13 @@ ggplot2::theme_set(ggplot2::theme_light())
 
 
 # Custom frequency table function
-table_frq <- function(data,var,digits=2){
+# 
+# @param data data frame object, passed implicitly
+# @param var variable name to use for frequency table
+# @param digits numeric round up to number of digits
+# @param fsize numeric font size for word tables
+# 
+table_frq <- function(data, var, digits=2, fsize=12){
   
   headertxt <- c("N", "Raw %", "Valid %", "Cum %")
   
@@ -46,7 +54,34 @@ table_frq <- function(data,var,digits=2){
   tblx <- tblx |>
     dplyr::select(-c(n_miss, n_valid)) |>
     dplyr::rename_with(~ headertxt, 2:5) |>
-    dplyr::mutate(across(where(is.numeric), round, digits))
+    dplyr::mutate(across(where(is.numeric), \(x) round(x, digits=digits)))
+  
+  
+  # convert variable object to string
+  label_str <-deparse(substitute(var))
+
+  # get label of the variable, i.e. the question wording
+  var_label <- attr(data[,label_str], which="label")  
+  
+
+  # word document
+  if (!knitr::is_html_output()) {
+
+    caption_txt <- var_label 
+    caption_auto <- officer::run_autonum(seq_id = "tab", 
+                                         pre_label = paste0("Table", " " ))
+    tnum_rows <- nrow(tblx)
+    missing_str <- "(Missing)"
+    fsize <- 12
+    
+    tblx <- flextable(tblx) |> 
+      flextable::font(fontname="Roboto", part="all") |> 
+      flextable::fontsize(size=fsize, part = "all") |>
+      flextable::bold(bold=T, part = "header") |> 
+      flextable::width(j=c(1:5), width=c(3,1,.6,1,1)) |>
+      flextable::set_caption(caption = caption_txt, autonum=caption_auto) |> 
+      flextable::compose(i = tnum_rows, j = 1, as_paragraph(as_chunk(missing_str)))
+  }
   
 
   tblx
